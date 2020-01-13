ca19abd72e3406101adb764fe1982d65ca523a53
Nikola Ljubešić <nljubesi@gmail.com>
2020-01-13 09:52:19 +0000

    Added Macedonian, moved to Python 3

--- tokeniser.py
+++ tokeniser.py
@@ -12,26 +12,27 @@ reldir=os.path.dirname(os.path.abspath(__file__))
 
 def read_abbrevs(file):
   abbrevs={'B':[],'N':[],'S':[]}
-  for line in open(os.path.join(reldir,file)):
+  for line in open(os.path.join(reldir,file),encoding='utf8'):
     if not line.startswith('#'):
-      abbrev,type=line.decode('utf8').strip().split('\t')[:2]
+      abbrev,type=line.strip().split('\t')[:2]
       abbrevs[type].append(abbrev)
   return abbrevs
 
 abbrevs={
   'hr':read_abbrevs('hr.abbrev'),
   'sr':read_abbrevs('sr.abbrev'),
-  'sl':read_abbrevs('sl.abbrev')
+  'sl':read_abbrevs('sl.abbrev'),
+  'mk':read_abbrevs('mk.abbrev')
 }
 
 num=r'(?:(?<!\d)[+-])?\d+(?:[.,:/]\d+)*(?:[.](?!\.)|-[^\W\d_]+)?'
 # emoswithspaces emoticon=r'[=:;8][\'-]*(?:\s?\)+|\s?\(+|\s?\]+|\s?\[+|\sd\b|\sp\b|d+\b|p+\b|s+\b|o+\b|/|\\|\$|\*+)|-\.-|\^_\^|\([\W]+\)|<3|</3|<\\3|\\o/'
 emoticon=r'[=:;8][\'-]*(?:\)+|\(+|\]+|\[+|d\b|p\b|d+\b|p+\b|s+\b|o+\b|/|\\|\$|\*+)|-\.-|\^_\^|\([^\w\s]+\)|<3|</3|<\\3|\\o/'
-word=ur'(?:[*]{2,})?\w+(?:[@­\'-]\w+|[*]+\w+)*(?:[*]{2,})?'
+word=r'(?:[*]{2,})?\w+(?:[@­\'-]\w+|[*]+\w+)*(?:[*]{2,})?'
 
 langs={
   'hr':{
-    'abbrev':ur'|'.join(abbrevs['hr']['B']+abbrevs['hr']['N']+abbrevs['hr']['S']),
+    'abbrev':r'|'.join(abbrevs['hr']['B']+abbrevs['hr']['N']+abbrevs['hr']['S']),
     'num':num,
     'url':r'https?://[-\w/%]+(?:[.#?=&@;][-\w/%]+)+|\b\w+\.(?:\w+\.)?(?:com|org|net|gov|edu|int|io|eu|si|hr|rs|ba|me|mk|it|at|hu|bg|ro|al|de|ch|be|dk|se|no|es|pt|ie|fr|fi|cl|co|bo|br|gr|ru|uk|us|by|cz|sk|pl|lt|lv|lu|ca|in|tr|il|iq|ir|hk|cn|jp|au|nz)/?\b',
     'htmlesc':r'&#?[a-z0-9]+;',
@@ -49,7 +50,7 @@ langs={
   },
 
   'sr':{
-    'abbrev':ur'|'.join(abbrevs['sr']['B']+abbrevs['sr']['N']+abbrevs['sr']['S']),
+    'abbrev':r'|'.join(abbrevs['sr']['B']+abbrevs['sr']['N']+abbrevs['sr']['S']),
     'num':num,
     'url':r'https?://[-\w/%]+(?:[.#?=&@;][-\w/%]+)+|\b\w+\.(?:\w+\.)?(?:[a-z]{3,4})/?\b',
     'htmlesc':r'&#?[a-z0-9]+;',
@@ -67,7 +68,25 @@ langs={
   },
 
   'sl':{
-    'abbrev':ur'|'.join(abbrevs['sl']['B']+abbrevs['sl']['N']+abbrevs['sl']['S']),
+    'abbrev':r'|'.join(abbrevs['sl']['B']+abbrevs['sl']['N']+abbrevs['sl']['S']),
+    'num':num,
+    'url':r'https?://[-\w/%]+(?:[.#?=&@;][-\w/%]+)+|\b\w+\.(?:\w+\.)?(?:[a-z]{3,4})/?\b',
+    'htmlesc':r'&#?[a-z0-9]+;',
+    'tag':r'</?[a-z][\w:]*>|<[a-z][\w:]*/?>',
+    'mail':r'[\w.-]+@\w+(?:[.-]\w+)+',
+    'mention':r'@[a-z0-9_]+',
+    'hashtag':r'#\w+(?:[.-]\w+)*',
+    'emoticon':emoticon,
+    'word':word,
+    'arrow':r'<[-]+|[-]+>',
+    'dot':r'[.!?/]{2,}',
+    'space':r'\s+',
+    'other':r'(.)\1*',
+    'order':('abbrev','num','url','htmlesc','tag','mail','mention','hashtag','emoticon','word','arrow','dot','space','other')
+  },
+
+  'mk':{
+    'abbrev':r'|'.join(abbrevs['mk']['B']+abbrevs['mk']['N']+abbrevs['mk']['S']),
     'num':num,
     'url':r'https?://[-\w/%]+(?:[.#?=&@;][-\w/%]+)+|\b\w+\.(?:\w+\.)?(?:[a-z]{3,4})/?\b',
     'htmlesc':r'&#?[a-z0-9]+;',
@@ -83,6 +102,7 @@ langs={
     'other':r'(.)\1*',
     'order':('abbrev','num','url','htmlesc','tag','mail','mention','hashtag','emoticon','word','arrow','dot','space','other')
   }
+
 }
 
 #transform abbreviation lists to sets for lookup during sentence splitting
@@ -104,7 +124,7 @@ def sentence_split_nonstd(tokens,lang):
   boundaries=[0]
   for index in range(len(tokens)-1):
     token=tokens[index][0]
-    if token[0] in u'.!?…': #if sentence ending punctuation
+    if token[0] in '.!?…': #if sentence ending punctuation
       boundaries.append(index+1)
     elif token.endswith('.'): #if abbreviation
       if token.lower() not in abbrevs[lang]['N']: #if not in non-splitting abbreviations
@@ -128,23 +148,23 @@ def sentence_split(tokens,lang):
   boundaries=[0]
   for index in range(len(tokens)-1):
     token=tokens[index][0]
-    if token[0] in u'.!?…' or (token.endswith('.') and token.lower() not in abbrevs[lang]['N'] and len(token)>2 and tokens[index+1][0][0] not in u'.!?…'):
+    if token[0] in '.!?…' or (token.endswith('.') and token.lower() not in abbrevs[lang]['N'] and len(token)>2 and tokens[index+1][0][0] not in '.!?…'):
       if tokens[index+1][0][0].isupper():
         boundaries.append(index+1)
         continue
       if index+2<len(tokens):
         if tokens[index+2][0][0].isupper():
-          if tokens[index+1][0].isspace() or tokens[index+1][0][0] in u'-»"\'':
+          if tokens[index+1][0].isspace() or tokens[index+1][0][0] in '-»"\'':
             boundaries.append(index+1)
             continue
       if index+3<len(tokens):
         if tokens[index+3][0][0].isupper():
-          if tokens[index+1][0].isspace() and tokens[index+2][0][0] in u'-»"\'':
+          if tokens[index+1][0].isspace() and tokens[index+2][0][0] in '-»"\'':
             boundaries.append(index+1)
             continue
       if index+4<len(tokens):
         if tokens[index+4][0][0].isupper():
-          if tokens[index+1][0].isspace() and tokens[index+2][0][0] in u'-»"\'' and tokens[index+3][0][0] in u'-»"\'':
+          if tokens[index+1][0].isspace() and tokens[index+2][0][0] in '-»"\'' and tokens[index+3][0][0] in '-»"\'':
             boundaries.append(index+1)
             continue
   boundaries.append(len(tokens))
@@ -199,8 +219,8 @@ def represent_tomaz(input,par_id):
 
 if __name__=='__main__':
   import argparse
-  parser=argparse.ArgumentParser(description='Tokeniser for (non-)standard Slovene, Croatian and Serbian')
-  parser.add_argument('lang',help='language of the text',choices=['sl','hr','sr'])
+  parser=argparse.ArgumentParser(description='Tokeniser for (non-)standard Slovene, Croatian, Serbian and Macedonian')
+  parser.add_argument('lang',help='language of the text',choices=['sl','hr','sr','mk'])
   parser.add_argument('-c','--conllu',help='generates CONLLU output',action='store_true')
   parser.add_argument('-b','--bert',help='generates BERT-compatible output',action='store_true')
   parser.add_argument('-d','--document',help='passes through ConLL-U-style document boundaries',action='store_true')
@@ -223,6 +243,6 @@ if __name__=='__main__':
         par_id=0
         sys.stdout.write(line)
         continue
-    sys.stdout.write(represent_tomaz(process[mode](tokenizer,line.decode('utf8'),lang),par_id).encode('utf8'))
+    sys.stdout.write(represent_tomaz(process[mode](tokenizer,line,lang),par_id))
     if args.bert:
       sys.stdout.write('\n')
